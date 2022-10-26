e14aba4eeefaecb63ea58b3146dd75152118ee36
cdufour <opusidea@gmail.com>
2022-10-26 13:51:10 +0200

    init

---
+++ Demo.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+/*
+Date: Octobre 2022
+Auteur: Christophe D.
+*/
+public class Demo
+{
+    public static void main(String[] args) {
+        System.out.println("Programme de demonstration");
+        System.out.println("Hello World!");
+        System.out.print("I am learning Java.");
+        System.out.println("It is awesome!");
+        //System.out.println(42 + 10);
+
+        // types de variable
+        int integer = 45630;
+        short sh = 42;
+        char character = 'c';
+        double db = 3.14;
+        float fl = 3.14f;
+        boolean bool = true;
+        // chaîne de caractères incluant une tabulation
+        String str = "Formation\tJava";
+        
+        System.out.println(str);
+
+        
+        // exemples d'opérations
+        short n1 = 2;
+        short n2 = 8;
+        int result = n1 + n2;
+        System.out.println(result);
+
+    }
+}
